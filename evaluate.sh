a991b450524443874393d1609a78934dc34757b0
Talley Lambert <talleylambert@NIC-Systems-iMac.local>
2015-05-19 13:32:04 -0400

    eval script

--- evaluate.sh
+++ evaluate.sh
@@ -6,11 +6,11 @@ function evaluate() {
 	N=$(expr ${#ARR[@]} - 1)
 	SCORES=()
 	for i in $(seq 1 3 $N); do 
-		a=$(expr $i);
-		b=$(expr $i + 1);
-		c=$(expr $i + 2);
-		SUM=$(bc <<< "${ARR[a]/,/} + ${ARR[b]/,/} + ${ARR[c]/,/}");
-		AVG=$(printf %.4f $(echo "$sum/3" | bc -l ) );
+		a=$(expr $i)
+		b=$(expr $i + 1)
+		c=$(expr $i + 2)
+		SUM=$(echo "${ARR[a]/,/} + ${ARR[b]/,/} + ${ARR[c]/,/}" | bc)
+		AVG=$(printf %.4f $(echo "$SUM/3" | bc -l ) )
 		SCORES+=($AVG);
 	done
 }
